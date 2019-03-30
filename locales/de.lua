9ed2c095f325425ee49bd99bf0f0cf75dc154309
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2019-03-30 01:13:10 +0100

    Create de.lua
    Translated EN to DE.

---
+++ locales/de.lua
@@ -0,0 +1,7 @@
+Locales['de'] = {
+
+	['new_job'] 			    = '<C>~o~Nun arbeitest Du für mich!</C>',
+	['access_job_list'] 	= 'Benutze ~INPUT_PICKUP~ um einen Illegalen Job anzunehmen~s~.',
+	['Lester_Jobs'] 		  = 'Zeitarbeit',
+
+}
